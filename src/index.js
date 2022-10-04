25371faa0d49510fa84b12539b74b44040ebab9a
Lucas Bide Machado <lucas.bidemachado@gmail.com>
2022-10-03 22:34:59 -0400

    Add tab icon

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ import './index.css';
 import App from './App';
 import { HashRouter, Routes, Route } from "react-router-dom";
 
-ReactDOM.render(
- <React.StrictMode>
- <HashRouter>
- <Routes>
- <Route path="/" element={<App/>} />
- </Routes>
- </HashRouter>
- </React.StrictMode>,
- document.getElementById("root")
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+  <HashRouter>
+  <Routes>
+  <Route path="/" element={<App/>} />
+  </Routes>
+  </HashRouter>
+  </React.StrictMode>
 );
 
